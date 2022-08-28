08f3562054e3839112b76f550aaef51917445468
gawryllah <gawrysjak@gmail.com>
2022-08-28 21:10:30 +0200

    added ball physics

--- brick-beater/Assets/Content/Scripts/BallController.cs
+++ brick-beater/Assets/Content/Scripts/BallController.cs
@@ -1,16 +1,97 @@
+using System.Collections;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rigidbody;
+    [SerializeField] float speed;
+    Vector2 direction;
+
+    private void Start()
     {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.AddForce(Vector2.down.normalized * speed * Time.deltaTime, ForceMode2D.Impulse);
 
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void FixedUpdate()
+    {
+        setMinimumVelocity();
+        setMaximumVelocity();
+        Debug.Log(Vector2.SqrMagnitude(rigidbody.velocity));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
     {
 
+        if (collision.gameObject.tag.Equals("Paddle"))
+        {
+            if (collision.collider.offset.x < 0)
+            {
+
+                rigidbody.AddForce((Vector2.left.normalized * 3f), ForceMode2D.Force);
+
+            }
+            else
+            {
+
+
+                rigidbody.AddForce((Vector2.right.normalized * 3f), ForceMode2D.Force);
+
+            }
+
+        }
+        else if (collision.gameObject.tag.Equals("Brick"))
+        {
+            rigidbody.velocity *= 1.02f;
+        }
     }
+
+    void setMinimumVelocity()
+    {
+        if (rigidbody.velocity.y > 0 && rigidbody.velocity.y < 0.35f)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, 1f);
+        }
+        else if (rigidbody.velocity.y < 0 && rigidbody.velocity.y > -0.35f)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, -1f);
+        }
+
+        if (rigidbody.velocity.x == 0)
+        {
+            StartCoroutine(horizontalCheck());
+        }
+    }
+
+    void setMaximumVelocity()
+    {
+        float maxSpeed = 300f;
+        float speed = Vector2.SqrMagnitude(rigidbody.velocity);
+
+        if (speed > maxSpeed)
+        {
+
+            rigidbody.velocity *= 0.95f;
+        }
+
+    }
+    
+
+    IEnumerator horizontalCheck()
+    {
+        Debug.Log($"At: {this}, started horizontalCheck, vel: {rigidbody.velocity}");
+        yield return new WaitForSeconds(5f);
+
+        if (rigidbody.velocity.x == 0)
+        {
+            rigidbody.velocity = new Vector2(Random.Range(-1f, 1f), rigidbody.velocity.y);
+        }
+
+        Debug.Log($"At: {this}, ended horizontalCheck, vel: {rigidbody.velocity}");
+    }
+
+
 }
---
+++ brick-beater/Assets/Content/Scripts/PaddleController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    [SerializeField] ControlsStats controlsStats;
+
+    private float height;
+
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    private void Start()
+    {
+        height = transform.position.y;
+    }
+
+
+    private void Update()
+    {
+
+        Holdball();
+        Move();
+    }
+
+
+
+    void Move()
+    {
+
+        if (transform.position.x > controlsStats.MaxDelta)
+        {
+            transform.position = new Vector2(controlsStats.MaxDelta, height);
+        }
+
+        if (transform.position.x < -controlsStats.MaxDelta)
+        {
+            transform.position = new Vector2(-controlsStats.MaxDelta, height);
+
+        }
+        else
+        {
+
+            var newPos = new Vector2(transform.position.x + (playerControls.Controls.Movement.ReadValue<Vector2>().x * controlsStats.Speed * Time.deltaTime), height);
+
+            transform.position = newPos;
+        }
+    }
+
+    void Holdball()
+    {
+        if (playerControls.Controls.Holdball.IsPressed())
+        {
+            Debug.Log($"at {this}, {playerControls.Controls.Holdball.name} {playerControls.Controls.Holdball.triggered}");
+        }
+    }
+}
