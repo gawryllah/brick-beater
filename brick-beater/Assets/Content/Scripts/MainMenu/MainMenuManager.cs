f36e734865cfa2ec95e3dcfc5b1858d0847ababe
gawryllah <gawrysjak@gmail.com>
2022-08-31 22:06:36 +0200

    redesigned main menu

--- brick-beater/Assets/Content/Scripts/MainMenu/MainMenuManager.cs
+++ brick-beater/Assets/Content/Scripts/MainMenu/MainMenuManager.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System.Collections;
 
 public class MainMenuManager : MonoBehaviour
 {
     private static MainMenuManager instance;
 
     public static MainMenuManager Instance { get { return instance; } }
+
+    [SerializeField] private GameObject logo;
+    [SerializeField] private TMP_Text hiScoreText;
+
+    private bool newGame;
 
     private void Awake()
     {
@@ -19,10 +26,50 @@ public class MainMenuManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(instance);
+
+        if (PlayerPrefs.HasKey("BB-HiScore"))
+        {
+            hiScoreText.text = $"HiScore: {PlayerPrefs.GetInt("BB-HiScore")}";
+        }
+    }
+
+    private void OnEnable()
+    {
+        newGame = false;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(logoAnim());
     }
 
     public void LoadGame()
     {
+        newGame = true;
+        StopAllCoroutines();
         SceneManager.LoadScene("GameScene");
+
+    }
+
+    public void InstructionView()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private IEnumerator logoAnim()
+    {
+        
+        while (!newGame)
+        {
+            yield return new WaitForSeconds(1.75f);
+            logo.SetActive(false);
+            yield return new WaitForSeconds(1f);
+            logo.SetActive(true);
+        }
     }
 }
